504157f7ac0600645a19abd5444378ca36544f97
Marcell Gyurján <gyurjan.marcell@gmail.com>
2024-03-13 22:31:20 +0100

    test accessing .gsheet content

--- Program.cs
+++ Program.cs
@@ -2,20 +2,41 @@
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
+using System.Text;
 
-namespace GoogleSheetsExample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace GoogleSheetsExample {
+    class Program {
+        static void Main(string[] args) {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
             string credentialsFileName = "japanese-417119-e94d80bf56c9.json";
+            string spreadsheetId = "1pJCzatA15yrxHMTlYcoxynaDEpdlMR07NNrmN462Ays";
+            string wordSheetName = "単語";
+            string readingSheetName = "読";
+
             string credentialsFilePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"..\\..\\..\\{credentialsFileName}"));
-            File.Exists(credentialsFilePath);
+
+            SheetsService service = CreateGoogleSheetsService(credentialsFilePath);
+
+            string range = $"{wordSheetName}!B3:B5";
+            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+            ValueRange response = request.Execute();
+        }
+
+        private static SheetsService CreateGoogleSheetsService(string credPath) {
+            GoogleCredential credential;
+
+            using (var stream = new FileStream(credPath, FileMode.Open, FileAccess.Read)) {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(SheetsService.Scope.Spreadsheets);
+            }
+
+            var service = new SheetsService(new BaseClientService.Initializer() {
+                HttpClientInitializer = credential,
+                ApplicationName = AppDomain.CurrentDomain.FriendlyName
+            });
+
+            return service;
         }
     }
 }
