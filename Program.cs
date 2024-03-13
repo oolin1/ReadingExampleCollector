bc9077583190e84f4062cb6eb4f011d72a56e1ec
Marcell Gyurján <gyurjan.marcell@gmail.com>
2024-03-13 21:30:24 +0100

    added google sheets nuget packages

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿namespace GoogleSheetsExample
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace GoogleSheetsExample
 {
     class Program
     {
