ec8b1caf365dea48319a4963739c260ec7ee08da
Marcell Gyurján <gyurjan.marcell@gmail.com>
2024-03-13 21:29:44 +0100

    moved .csproj next to .sln, located credentials .json

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GoogleSheetsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string credentialsFileName = "japanese-417119-e94d80bf56c9.json";
+            string credentialsFilePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"..\\..\\..\\{credentialsFileName}"));
+            File.Exists(credentialsFilePath);
+        }
+    }
+}
