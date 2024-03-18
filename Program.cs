aa3a0e79e7c6ca319381dbd1f43d67204c770e60
Marcell Gyurján <gyurjan.marcell@gmail.com>
2024-03-18 22:44:04 +0100

    reading in all of the words (multiple ranges)

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using System.Text;
+using GetRequest = Google.Apis.Sheets.v4.SpreadsheetsResource.ValuesResource.GetRequest;
 
 namespace GoogleSheetsExample {
     class Program {
@@ -10,18 +11,30 @@ namespace GoogleSheetsExample {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
+            List<string> ranges = new List<string> { "B2:B1800", "J2:J1800" };
+
             string credentialsFileName = "japanese-417119-e94d80bf56c9.json";
             string spreadsheetId = "1pJCzatA15yrxHMTlYcoxynaDEpdlMR07NNrmN462Ays";
             string wordSheetName = "単語";
             string readingSheetName = "読";
 
             string credentialsFilePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"..\\..\\..\\{credentialsFileName}"));
-
             SheetsService service = CreateGoogleSheetsService(credentialsFilePath);
 
-            string range = $"{wordSheetName}!B3:B5";
-            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
-            ValueRange response = request.Execute();
+            List<string> words = new List<string>();
+
+            foreach (string range in ranges) {
+                string reqRange = $"{wordSheetName}!{range}";
+
+                GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, reqRange);
+                ValueRange response = request.Execute();
+
+                
+                var newWords = response.Values.Select(x => x.FirstOrDefault()?.ToString()).Where(x => x != null && x != "-");
+                words.AddRange(newWords);
+            }
+
+            words = words.Distinct().ToList();
         }
 
         private static SheetsService CreateGoogleSheetsService(string credPath) {
